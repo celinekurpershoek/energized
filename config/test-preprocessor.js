7cefe8d11f1ba227e65e7daa5a19200b542c7a95
Celine Kurpershoek <celinekurpershoek@gmail.com>
2019-03-19 15:30:29 +0100

    fix(tests): fix tests on ci

--- config/test-preprocessor.js
+++ config/test-preprocessor.js
@@ -4,7 +4,7 @@ const tsConfig = require("../tsconfig.json");
 module.exports = {
   process(src, path) {
     if (path.endsWith(".ts") || path.endsWith(".tsx") || path.endsWith(".js")) {
-      return tsc.transpile("__test__", tsConfig.compilerOptions, path, []);
+      return tsc.transpile(src, tsConfig.compilerOptions, path, []);
     }
     return src;
   }
